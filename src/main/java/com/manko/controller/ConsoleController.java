1cfd63d631cc605475fce31347d99e476388901f
Zhenyya <jane07oks@gmail.com>
2020-11-10 14:04:46 +0200

    Created enum NeedCard, changed class ConsoleController

--- src/main/java/com/manko/controller/ConsoleController.java
+++ src/main/java/com/manko/controller/ConsoleController.java
@@ -1,6 +1,11 @@
 package com.manko.controller;
 
+import com.manko.model.enums.EmotionCard;
+import com.manko.model.enums.NeedCard;
 import com.manko.view.ConsoleView;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Yevheniia Manko on 09.11.2020.
@@ -18,6 +23,37 @@ public class ConsoleController {
         do {
             userChoice = view.start();
             System.out.println("Your choice is " + userChoice);
+            switch (userChoice) {
+                case "1":
+                case "2":
+                    getSetOfCards(userChoice);
+                    break;
+                case "3":
+                    // some code
+                    break;
+                case "4":
+                    // some code
+                    break;
+                case "Q":
+                    break;
+                default:
+                    System.out.println("Invalid input. try again");
+            }
         } while(!userChoice.equals("Q"));
+    }
+
+    private Set<String> getSetOfCards(String str) {
+        Set<String> cards = new HashSet<>();
+        if(str.equals("1")) {
+            for (EmotionCard emotion : EmotionCard.values()) {
+                cards.add(emotion.getNameUkr() + "\n" + emotion.name() + "\n" + emotion.getNameRus());
+            }
+        }
+        if(str.equals("2")) {
+            for (NeedCard emotion : NeedCard.values()) {
+                cards.add(emotion.getNameUkr() + "\n" + emotion.name() + "\n" + emotion.getNameRus());
+            }
+        }
+        return cards;
     }
 }
---
+++ src/main/java/com/manko/model/enums/NeedCard.java
@@ -0,0 +1,16 @@
+package com.manko.model.enums;
+
+/**
+ * Created by Yevheniia Manko on 10.11.2020.
+ */
+public enum NeedCard {
+    ;
+
+    public String getNameUkr() {
+        return null;
+    }
+
+    public String getNameRus() {
+        return null;
+    }
+}
