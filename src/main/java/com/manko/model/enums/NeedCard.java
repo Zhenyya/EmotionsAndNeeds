3c2857a88c21a261a0faac4aeb7b2a9106870846
Zhenyya <jane07oks@gmail.com>
2020-11-12 21:44:28 +0200

    Updated NeedCard file

--- src/main/java/com/manko/model/enums/NeedCard.java
+++ src/main/java/com/manko/model/enums/NeedCard.java
@@ -4,6 +4,7 @@ package com.manko.model.enums;
  * Created by Yevheniia Manko on 10.11.2020.
  */
 public enum NeedCard {
+    // I will add names of needs here
     ;
 
     public String getNameUkr() {
