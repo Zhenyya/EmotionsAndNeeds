c1e9584c40244c55b926828e9b36529719c52f50
Zhenyya <jane07oks@gmail.com>
2021-08-11 10:02:01 +0300

    Changed EmotionalState class

--- src/main/java/com/manko/model/EmotionalState.java
+++ src/main/java/com/manko/model/EmotionalState.java
@@ -1,7 +1,15 @@
 package com.manko.model;
 
+import com.manko.model.enums.EmotionCard;
+import com.manko.model.enums.NeedCard;
+
+import java.util.HashSet;
+
 /**
  * Created by Yevneniia Manko on 11.08.2021.
  */
 public class EmotionalState {
+
+    private HashSet<EmotionCard> emotions;
+    private HashSet<NeedCard> needs;
 }
